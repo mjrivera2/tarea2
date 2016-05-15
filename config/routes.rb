6e1d2c9b151b329049dd4708e1d80ff39ea2f0d4
Jesu <mjrivera2@puc.cl>
2016-05-15 18:04:19 -0300

    Fix route. Version 1.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-  post '/' => 'application#buscar'
+  post '/instagram/tag/buscar' => 'application#buscar'
   
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
